21319b4316d4f9469c8a7718db97a68fa4ee1920
root <root@Gustavo.localdomain>
2026-02-17 18:47:37 -0300

    Add ffw sketch

--- model.py
+++ model.py
@@ -126,6 +126,31 @@ class SelfAttention(nn.Module):
         pre_out = attended.squeeze(2).view(B, self.n_heads_q * self.head_dim)
 
         return self.wo(pre_out)
+    
+class FeedForward(nn.Module):
+    def __init__(self, args: ModelArgs):
+        super().__init__()
+
+        hidden_dim = 4 * args.dim
+        hidden_dim = int(2 * hidden_dim / 3)
+
+        if args.ffn_dim_multiplier is not None:
+            hidden_dim = int(hidden_dim * args.ffn_dim_multiplier)
+
+        hidden_dim = args.multiple_of * ((hidden_dim + args.multiple_of - 1) // args.multiple_of)
+        
+        self.w1 = nn.Linear(args.dim, hidden_dim, bias=False)
+        self.w2 = nn.Linear(hidden_dim, args.dim, bias=False)
+        self.w3 = nn.Linear(args.dim, hidden_dim, bias=False)
+    
+    def forward(self, x: torch.Tensor):
+        # B, T, dim -> B, T, x*dim
+        swish = F.silu(self.w1(x))
+        x_V = self.w3(x)
+        
+        x = self.w2(swish * x_V)
+        
+        return x
 
 class EncoderBlock(nn.Module):
     def __init__(self, args: ModelArgs):
